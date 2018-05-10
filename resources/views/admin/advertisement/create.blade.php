4c4f64a0d6fe5bb8582c94cc12d9a4711ce80ee0
Phuc Long <longnp95@gmail.com>
2018-05-11 00:45:27 +0700

    fix ads bi adblock block

--- resources/views/admin/advertisement/create.blade.php
+++ resources/views/admin/advertisement/create.blade.php
@@ -25,7 +25,7 @@
                                        name="files[]"
                                        class="fileupload"
                                        accept="image/*"
-                                       data-upload-url="/admin/api/uploadimage/ads"
+                                       data-upload-url="/admin/api/uploadimage/slider"
                                        data-target="#url_vn"
                                        data-delete-button="#cover-remove-vn"
                                        data-preview="#cover-preview-vn">
@@ -57,7 +57,7 @@
                                        name="files[]"
                                        class="fileupload"
                                        accept="image/*"
-                                       data-upload-url="/admin/api/uploadimage/ads"
+                                       data-upload-url="/admin/api/uploadimage/slider"
                                        data-target="#url_jp"
                                        data-delete-button="#cover-remove-jp"
                                        data-preview="#cover-preview-jp">
